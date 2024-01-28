80fa665003bbdd58109555f9eab7f23a90868c5a
Rafaele Muniz <rafaeleschmoeller@gmail.com>
2024-01-28 18:38:43 -0300

    "Modelando Screenmatch"

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,54 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroDeRecomendacao;
+import br.com.alura.screenmatch.modelos.Epsodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.duracaoEmMinutos = 175;
+        meuFilme.setNome("Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(9);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2004);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(6);
+        lost.setEpisodiosPorTemporada(23);
+        lost.setMinutosPorEpisodios(40);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos() + " min.");
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2009);
+        outroFilme.setDuracaoEmMinutos(162);
+
+        CalculadoraDeTempo caluladora = new CalculadoraDeTempo();
+        caluladora.inclui(meuFilme);
+        caluladora.inclui(outroFilme);
+        caluladora.inclui(lost);
+        System.out.println(caluladora.getTempoTotal());
+
+        FiltroDeRecomendacao filtro = new FiltroDeRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Epsodio epsodio = new Epsodio();
+        epsodio.setNumero(1);
+        epsodio.setSerie(lost);
+        epsodio.setTotalVisualizacoes(300);
+        filtro.filtra(epsodio);
+
+
 
     }
 }
